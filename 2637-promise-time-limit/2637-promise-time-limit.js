b49f3b9e29d98a7c3d5390f29cfed2b6fd7da048
suhani <suhaanii643@gmail.com>
2026-07-21 21:22:31 +0530

    Time: 59 ms (14.54%), Space: 54.6 MB (21.62%) - LeetHub

---
+++ 2637-promise-time-limit/2637-promise-time-limit.js
@@ -0,0 +1,9 @@
+const timeLimit = function(fn, t) {
+    return async function(...args) {
+        const timeoutPromise = new Promise((_, reject) => {
+            setTimeout(() => reject("Time Limit Exceeded"), t);
+        });
+
+        return Promise.race([fn(...args), timeoutPromise]);
+    };
+};
